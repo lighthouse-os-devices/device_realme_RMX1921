bb4db706614fd4ea9b59e4f3e7674b7aa56407d1
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-08-04 19:48:04 +0530

    RMX1921: fixup some props in mk flags
    Signed-off-by: Deepak5310 <deepakjangir5310@gmail.com>

--- lighthouse_RMX1921.mk
+++ lighthouse_RMX1921.mk
@@ -15,13 +15,14 @@ PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_NAME := lighthouse_RMX1921
-PRODUCT_MODEL := XT
+PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
-    PRODUCT_NAME="RMX1921"
+    PRODUCT_NAME="RMX1921" \
+    TARGET_DEVICE="RMX1921"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
