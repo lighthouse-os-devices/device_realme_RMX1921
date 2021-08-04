d6fe3318e83017ca7a2eaca51956af717a72c457
Deepak5310 <deepakjangir5310@gmail.com>
2021-08-04 19:25:45 +0530

    RMX1921: Lighthouse Bringup
    Signed-off-by: Deepak5310 <deepakjangir5310@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1921.mk
+    $(LOCAL_DIR)/lighthouse_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1921-userdebug \
-    lineage_RMX1921-user \
-    lineage_RMX1921-eng
+    lighthouse_RMX1921-userdebug \
+    lighthouse_RMX1921-user \
+    lighthouse_RMX1921-eng
--- lighthouse_RMX1921.mk
+++ lighthouse_RMX1921.mk
@@ -5,7 +5,8 @@
 #
 
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lighthouse/config/common_full_phone.mk)
+LIGHTHOUSE_BUILD_TYPE := OFFICIAL
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -13,7 +14,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := lineage_RMX1921
+PRODUCT_NAME := lighthouse_RMX1921
 PRODUCT_MODEL := XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
